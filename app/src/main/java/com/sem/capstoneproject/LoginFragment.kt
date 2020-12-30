53f94c61a10a39426247f68b9cf5861ca300a671
Sem <sembijpost@gmail.com>
2020-12-30 16:19:53 +0100

    sign in functie gemaakt

---
+++ app/src/main/java/com/sem/capstoneproject/LoginFragment.kt
@@ -0,0 +1,73 @@
+package com.sem.capstoneproject
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_login.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class LoginFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        auth = Firebase.auth
+
+        
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        loginButton.setOnClickListener {
+            if(tietEmail.text.isNullOrBlank() or tietPassword.text.isNullOrBlank()) {
+                Snackbar.make(view, "Please fill in both fields.", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            } else {
+                signInUser(view, tietEmail.text.toString(), tietPassword.text.toString())
+            }
+        }
+    }
+
+    fun signInUser(view: View, email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Snackbar.make(view, "Authentication failed.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+                    //updateUI(null)
+                }
+            }
+    }
+
+
+
+}
